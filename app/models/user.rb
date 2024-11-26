e9063410990ccb5c73b2da7e4788e5707c0bd8b8
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2024-11-26 06:53:14 +0000

    [Add]フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_one_attached :profile_image
+  
+
+  has_many :following_relationships, class_name: "Relationship", foreign_key: 'follower_id', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+  
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: 'following_id', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+
+  
+  
   
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -18,4 +29,26 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def is_followed_by?(user)
+    follower_relationships.find_by(following_id: user.id).present?
+  end
+
+  def follow(user_id)
+    transaction do
+      following_relationships.create(following_id: user_id)
+    end
+  end
+
+  def unfollow(user_id)
+    transaction do
+      following_relationships.find_by(following_id: user_id).destroy
+    end
+  end
+  
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
